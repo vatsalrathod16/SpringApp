d987357aac8f88a1425b524958526ff5ef836685
Vatsal Rathod <vatsalrathod16@gmail.com>
2023-08-13 23:53:17 -0400

    CRUD operations

--- JPAdemo/src/main/java/com/jpa/test/JpAdemoApplication.java
+++ JPAdemo/src/main/java/com/jpa/test/JpAdemoApplication.java
@@ -1,5 +1,8 @@
 package com.jpa.test;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -14,14 +17,52 @@ public class JpAdemoApplication {
 		ApplicationContext context = SpringApplication.run(JpAdemoApplication.class, args);
 		UserRepository userRepository = context.getBean(UserRepository.class);
 		
-		User user = new User();
-		user.setName("Vatsal Rathod");
-		user.setCity("Mumbai");
-		user.setStatus("I am java developer");
+//		User user = new User();
+//		user.setName("Vatsal Rathod");
+//		user.setCity("Mumbai");
+//		user.setStatus("I am java developer");
+//		
+//		User user1 = userRepository.save(user);
+//		
 		
-		User user1 = userRepository.save(user);
+		//create
+//		System.out.println(user1);
+//		User user1 = new User();
+//		user1.setName("Aayush Rathod");
+//		user1.setCity("Mumbai");
+//		user1.setStatus("I am python developer");
+//		
+//		User user2 = new User();
+//		user2.setName("Noopur Rathod");
+//		user2.setCity("Mumbai");
+//		user2.setStatus("I am js developer");
+//		List<User> users = List.of(user1,user2);
+//		Iterable<User> result = userRepository.saveAll(users);
+//		
+//		
+//		result.forEach(user->{
+//			System.out.println(user);
+//		});
+//		User resultUser = userRepository.save(user2);
+//		System.out.println(resultUser);
 		
-		System.out.println(user1);
+		//update by id & read
+//		Optional<User> optional = userRepository.findById(52);
+//		User user = optional.get();
+//		
+//		user.setName("Kinjal Rathod");
+//		User result = userRepository.save(user);
+//		
+//		System.out.println(result);
+		
+		//delete by id
+//		userRepository.deleteById(52);
+//		System.out.println("deleted");
+		
+		//delete all
+		Iterable<User> allusers = userRepository.findAll();
+		allusers.forEach(users->System.out.println(users));
+		userRepository.deleteAll(allusers);
 	}
 
 }
